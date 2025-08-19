31b6a1bc06e992023265d4676ab4b1107f9ac7be
Miles <toyotajochen@users.noreply.github.com>
2025-08-19 17:00:01 +0200

    small fixes for last commit

--- include/Algorithms.h
+++ include/Algorithms.h
@@ -10,7 +10,7 @@ namespace Algorithms {
     std::vector<std::vector<uint32_t>> enumerate_initial_non_it(AF & af, const IterableBitSet & active_arguments);
     std::vector<std::vector<uint32_t>> enumerate_procedural(AF & af, const IterableBitSet & active_arguments);
     std::set<uint32_t> characteristicFunction(AF & af, IterableBitSet active_arguments, std::set<uint32_t> & arguments);
-    bool contains_admissible(AF & af, std::set<uint32_t> & arguments, int arg);
+    bool contains_admissible(AF & af, IterableBitSet active_arguments, std::set<uint32_t> & arguments, int arg);
     bool is_initial(AF & af, std::set<uint32_t> & arguments);
 }
 
--- src/ContainsADM.cpp
+++ src/ContainsADM.cpp
@@ -2,11 +2,11 @@
 #include "Algorithms.h"
 
 namespace Algorithms{
-    bool contains_admissible(AF & af, std::set<uint32_t> & arguments, int arg){
+    bool contains_admissible(AF & af, IterableBitSet active_arguments, std::set<uint32_t> & arguments, int arg){
         if(std::find(arguments.begin(), arguments.end(), arg) == arguments.end()){
             return false;
         }
-        std::set<uint32_t> charFunc = characteristicFunction(af, arguments);
+        std::set<uint32_t> charFunc = characteristicFunction(af, active_arguments, arguments);
         if(std::includes(charFunc.begin(), charFunc.end(), arguments.begin(), arguments.end())){
             return true;
         } else {
@@ -16,7 +16,7 @@ namespace Algorithms{
             if(intersection.empty()) {
                 return false;
             } else {
-                return contains_admissible(af, intersection, arg);
+                return contains_admissible(af, active_arguments, arguments, arg);
             }
         }
     }
